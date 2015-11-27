93c8359296b831de07597f69f9a5393bd8923a24
slasheks <xhrios@gmail.com>
2015-11-26 23:07:22 -0500

    parser fix

--- langs/python/tests/segmain.py
+++ langs/python/tests/segmain.py
@@ -4,11 +4,11 @@ import argparse
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser(description='hey')
-  parser.add_argument('-cidr', dest='cidr', help='enter cidr!')
+  parser.add_argument('-cidr', dest='cidr', help='Please enter a CIDR (ex: 192.168.1.0/24)')
   args = parser.parse_args()
 
   if not args.cidr:
-    print "Please enter a CIDR (ex: 192.168.1.0/24)"
+    parser.print_help()
     exit(1)
 
   seg_builder(args.cidr)
