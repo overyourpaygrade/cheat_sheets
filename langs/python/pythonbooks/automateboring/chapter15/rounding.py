94cbbae5d3dae02b4117a9ad3725cf83c0fa2bb2
slasheks <xhrios@gmail.com>
2015-11-28 16:07:02 -0500

    chapter 15 time stuff

---
+++ langs/python/pythonbooks/automateboring/chapter15/rounding.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import time
+now = time.time()
+
+print(now)
+
+print(round(now, 2))
+
+print(round(now, 4))
+
+print(round(now))
---
+++ langs/python/pythonbooks/automateboring/chapter15/timer_more.py
@@ -0,0 +1,12 @@
+import time
+
+for i in range(3):
+  print('Tick')
+  time.sleep(1)
+  print('Tock')
+  time.sleep(1)
+
+time.sleep(5)
+
+for i in range(30):
+  time.sleep(1)
