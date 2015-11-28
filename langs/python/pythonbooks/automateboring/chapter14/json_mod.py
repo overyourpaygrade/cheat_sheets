1408704a1799cf23d5ca1b80fb3d6df23a89c997
slasheks <xhrios@gmail.com>
2015-11-28 15:35:33 -0500

    Added example delimiters

--- langs/python/pythonbooks/automateboring/chapter14/json_mod.py
+++ langs/python/pythonbooks/automateboring/chapter14/json_mod.py
@@ -1,3 +1,5 @@
+print "\nExample 1 - Reading JSON with the loads() function"
+
 stringOfJsonData = '{"name": "Zophie", "isCat": true, "miceCaught": 0, "felineIQ": null}'
 import json
 jsonDataAsPythonValue = json.loads(stringOfJsonData)
@@ -6,6 +8,8 @@ print jsonDataAsPythonValue.__class__
 '''
 {u'miceCaught': 0, u'isCat': True, u'felineIQ': None, u'name': u'Zophie'}
 '''
+
+print "\nExample 2 - Writing JSON with the dumps() function" 
 
 pythonValue  = '{"name": "Zophie", "isCat": true, "miceCaught": 0, "felineIQ": null}'
 stringOfJsonData = json.dumps(pythonValue)
