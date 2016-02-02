84da227d68260f3fc7220a42051073f533597807
slasheks <xhrios@gmail.com>
2016-02-02 18:18:59 -0500

    added logging

--- langs/python/tests/dev_db/read_db.py
+++ langs/python/tests/dev_db/read_db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sqlite3, sys, argparse, csv
-
+import logging
 
 def main():
 
@@ -14,23 +14,36 @@ def main():
 
     # Get arguments
     parser = argparse.ArgumentParser(description='hey')
-    parser.add_argument('-opts', dest='opts', help='something')
-    parser.add_argument('-query', dest='query', help='something')
+    parser.add_argument('-o', dest='opts', help='option')
+    parser.add_argument('-q', dest='query', help='query')
+    parser.add_argument('-l', dest='debug', help='debug')
     args = parser.parse_args()
     
     # At least one argument check
     if not args.opts:
         print "Enter an option (all, host _hostname_, join)"
         sys.exit(1)
+
+    # Set logging parameters
+    if args.debug:
+
+        LEVELS = { 'debug': logging.DEBUG,
+                   'info' : logging.INFO
+                 }
+        level = LEVELS.get(args.debug.lower(), logging.NOTSET)
+        logging.basicConfig(level=level)
+        logger = logging.getLogger(__name__)
     
     # Dump all in selected table
-    if args.opts == "all":
+    if args.opts == "all" and args.query:
+        logging.debug("Option: {} -- Query: {}".format(args.opts,args.query))
         for row in cur.execute('SELECT * FROM {}'.format(args.query)):
             print(row)
 
     # Select host from Hosts table
-    elif args.opts == "host":
-        for row in cur.execute('SELECT * FROM Hosts WHERE Hostname=?', (args.query,)):
+    elif args.opts == "host" and args.query:
+        logging.debug("Option: {} -- Query: {}".format(args.opts,args.query))
+        for row in cur.execute('SELECT * FROM Hosts WHERE Hostname="{}"'.format(args.query)):
     	    print(row)
 
     # Join tables on key and print
@@ -43,6 +56,7 @@ def main():
     elif args.opts == "writer":
         data = cur.execute('SELECT Location, Hostname, Position, Data FROM Hosts \
             INNER JOIN Locations ON Hosts.Location = Locations.Location_fk')
+        logging.debug("Data: {}".format(data))
 
         with open('output.csv', 'wb') as f:
             writer = csv.writer(f)
