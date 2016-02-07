da9e032a80298c72ca0a68898970c948fdb135d1
slasheks <xhrios@gmail.com>
2016-02-07 13:21:19 -0500

    fixed python steg pic with PIL

---
+++ langs/python/forensicpy/stego_pic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import PIL.Image
+
+with open('secret.png', 'rb') as fp:
+
+    img = PIL.Image.open(fp)
+
+    width,height = img.size
+
+    #print("width: {} -- height: {}".format(width,height))
+
+    newIm = PIL.Image.new("RGB", (width,height))
+
+    for row in range(height):
+
+        for col in range(width):
+
+            fgr, fgb, fgg = img.getpixel((col,row))
+
+            if fgr % 2 == 0:
+                newIm.putpixel((col,row),(255,255,255))
+            else:
+                newIm.putpixel((col,row),(0,0,0))
+
+    newIm.save('out.png')
+
