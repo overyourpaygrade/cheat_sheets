164a743d64ae7944b5f67f9f0a3f23159c41eaf5
slasheks <xhrios@gmail.com>
2016-02-11 22:25:27 -0500

    Anonymize examples

---
+++ langs/python/pythonbooks/violentpython/chapter6/userAgentTest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import mechanize
+
+def testUserAgent(url,userAgent):
+    browser = mechanize.Browser()
+    browser.addheaders = userAgent
+    page = browser.open(url)
+    source_code = page.read()
+    print source_code
+
+url = 'http://whatsmyuseragent.com/'
+
+userAgent = [('User-agent','Mozilla/5.0 (X11; U; '+\
+    'Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01')]
+
+testUserAgent(url,userAgent)
