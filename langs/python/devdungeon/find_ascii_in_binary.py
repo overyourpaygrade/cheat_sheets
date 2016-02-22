bdee7ea230f8bccd6ff35f01fafc7c26df6f49b4
slasheks <xhrios@gmail.com>
2016-02-21 21:48:54 -0500

    spacing!

--- langs/python/devdungeon/find_ascii_in_binary.py
+++ langs/python/devdungeon/find_ascii_in_binary.py
@@ -8,7 +8,7 @@ chunk_size = 1
 with open(sys.argv[1], 'rb') as in_file:
     for data in iter(partial(in_file.read, chunk_size), b''):
         x = int.from_bytes(data, byteorder='big')
-        if x > 64 and x < 91) or ( x > 96and x < 123):
+        if x > 64 and x < 91) or ( x > 96 and x < 123):
             sys.stdout.write(chr(x))
         else:
             sys.stdout.write('.')
