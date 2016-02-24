66aa33418cfe6718d20cd1567372301676343241
slasheks <xhrios@gmail.com>
2016-02-24 09:15:18 -0500

    instance attributes

---
+++ langs/python/pythonbooks/oreilly_beyond_basics/0304_instance_attrib.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import random
+
+class MyClass(object):
+    def dothis(self):
+        self.rand_val = random.randint(1,10)
+
+myinst = MyClass()
+
+myinst.dothis()
+
+print(myinst.rand_val)
