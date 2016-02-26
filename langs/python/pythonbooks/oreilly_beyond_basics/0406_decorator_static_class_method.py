6086c2da8d75ae4dc3e61bbd0ebeb487ada9305a
slasheks <xhrios@gmail.com>
2016-02-25 21:45:33 -0500

    fixes and changes

--- langs/python/pythonbooks/oreilly_beyond_basics/0406_decorator_static_class_method.py
+++ langs/python/pythonbooks/oreilly_beyond_basics/0406_decorator_static_class_method.py
@@ -1,18 +1,28 @@
 #!/usr/env/bin python
 
-class InstanceCounters(object):
+class InstanceCounter(object):
     count = 0
 
     def __init__(self, val):
         self.val = val
-        InstanceCounter += 1
+        InstanceCounter.count += 1
 
     def set_val(self, newval):
         self.val = newval
 
     def get_val(self):
-        return InstanceCounter.count
+        return self.val
+
+    @classmethod
+    def get_count(cls):
+        return cls.count
 
 a = InstanceCounter(5)
 b = InstanceCounter(13)
 c = InstanceCounter(17)
+
+for obj in (a,b,c):
+    print "val of obj: %s" % (obj.get_val()) # init value (5, 13, etc)
+    print "count: %s" % (obj.get_count())    # always 3
+
+print InstanceCounter.get_count()
