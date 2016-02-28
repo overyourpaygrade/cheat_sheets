a91b239beb3ddde82310dbc4b84b549c3412f37b
slasheks <xhrios@gmail.com>
2016-02-28 17:11:38 -0500

    05 examples

---
+++ langs/python/pythonbooks/oreilly_beyond_basics/0505_with_context2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+class MyClass(object):
+
+    def __enter__(self):
+        print('we have entered "with"')
+        return self
+
+    def __exit__(self, type, value, traceback):
+        print('error type: {0}'.format(type))
+        print('error value: {0}'.format(value))
+        print('error traceback: {0}'.format(traceback))
+
+    def sayhi(self):
+        print('hi, instance %s' % (id(self)))
+
+
+with MyClass() as cc:
+    cc.sayhi()
+
+print('after the "with" block')
