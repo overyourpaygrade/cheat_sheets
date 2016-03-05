1eed841228d5cae994bd40f7e0bc2283e93785f1
slasheks <xhrios@gmail.com>
2016-03-05 17:29:03 -0500

    decorator examples

---
+++ langs/python/tests/decorators/decorate_func_manual.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def get_text(name):
+    return "lorem ipsum, {0} dolor sit amet".format(name)
+
+def p_decorate(func):
+    def func_wrapper(name):
+        return "<p>{0}</p>".format(func(name))
+    return func_wrapper
+
+my_get_test = p_decorate(get_text)
+print my_get_test("John")
+
+get_text = p_decorate(get_text)
+print get_text("Jill")
+
+
+
