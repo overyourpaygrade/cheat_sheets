34b5ef18b69a41a902086d422dc9b388f8a293ef
slasheks <xhrios@gmail.com>
2016-03-13 22:54:06 -0400

    inventory parser

---
+++ langs/python/tests/networking/juniper/inventory.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import yaml
+import xmltodict
+
+with open('inventory.yml','rb') as f_yaml, open('inventory.xml') as fd:
+    yaml_settings = yaml.load(f_yaml)
+    print('YAML file:\n')
+    print(yaml_settings)
+    #print(yaml_settings[0]['name'])
+    #print(yaml_settings[0]['ports'])
+
+    #for line in yaml_settings[0]['ports']['10g']:
+    #    print line
+
+    mydict = xmltodict.parse(fd.read())
+
+    for mod in mydict['rpc-reply']['chassis-inventory']['chassis']['chassis-module']:
+        #print "{}  {}".format(mod['name'],mod['description'])
+        if "FPC" in mod['name']:
+            print mod['name']
+            print mod['model-number']
+
+            for line in mod['chassis-sub-module']:
+                print "Name: {}".format(line['name'])
+                print "Desc: {}".format(line['description'])
+                print "PNum {}".format(line['part-number'])
+
+                if 'chassis-sub-sub-module' in line:
+                    for fk in line['chassis-sub-sub-module']:
+                        if not isinstance(fk, basestring):
+                            for lin in yaml_settings[3]['pics'][0]['ports']['10g']:
+                                match = 0
+                                #print "this is fk", fk
+                                if lin in fk['name']:
+                                    print "    subs:{} == {}".format(lin,fk['name'])
+                                    match = 1
+                                #if match == 0:
+                                #    print "    subs:{} == None".format(lin)
+                        else:
+                            li = line['chassis-sub-sub-module']['name']
+                            for lin in yaml_settings[3]['pics'][0]['ports']['10g']:
+                                if li in lin:
+                                    print "    subs:{} == {}".format(lin,li)
+                                else:
+                                    print "    subs:{} == None".format(lin)
+                print
+
+
