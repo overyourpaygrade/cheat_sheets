757f7285594dc98bf2d8d049b70aa86cc9df3043
slasheks <xhrios@gmail.com>
2016-03-15 22:48:47 -0400

    more changes

--- langs/python/tests/networking/juniper/net_inv_etree.py
+++ langs/python/tests/networking/juniper/net_inv_etree.py
@@ -14,23 +14,40 @@ with open('inventory.yml','rb') as f_yaml, open('inventory.xml') as f_xml:
 
     tag1 = root[0].tag
 
-    t_g = '{http://xml.juniper.net/junos/12.3R6/junos-chassis}name'
-    for node in tree.iter(tag='{}'.format(t_g)):
-        print node.tag, node.attrib
-        for nd in node:
-            print nd.text
+    t_g = '{http://xml.juniper.net/junos/12.3R6/junos-chassis}chassis-module'
+    #for node in tree.iter(tag='{}'.format(t_g)):
+    #    # All I need is the text of the tag
+    #    # print node.tag, node.attrib, node.text
+    #    #print node.text  # Empty print
+    #    #print node # prints 5. there are 5 module tags
 
-    tg = '{http://xml.juniper.net/junos/12.3R6/junos-chassis}chassis-sub-sub-module'
-    for n in tree.iter(tag='{}'.format(tg)):
-        for oh in n:
-            print oh.text
+    #    for nd in node.iter():
+    #        print nd # Prints all the categories
 
-    for p in tree.iter(tag='{*}name'):
-        print dir(p)
+    ## Prints submodule
+    #tg = '{http://xml.juniper.net/junos/12.3R6/junos-chassis}chassis-sub-sub-module'
+    #for n in tree.iter(tag='{}'.format(tg)):
+    #    for oh in n:
+    #        print oh.text
 
-    tg2 = '{http://xml.juniper.net/junos/12.3R6/junos-chassis}'
-    for elem in tree.iterfind('{}'.format(tg2)):
-        print elem
+
+    # Does work
+    #tg2 = '{http://xml.juniper.net/junos/12.3R6/junos-chassis}name'
+    #for elem in tree.iter('{}'.format(tg2)):
+    #    print elem
+
+    # Does not work
+    #all_items = tree.findall('{}'.format(tg2))
+    #for item in all_items:
+    #    print dir(item)
+
+    # Does not work
+    data = tree.find('{}'.format(t_g))
+    for da in data:
+        print da
+
+    #for p in tree.find('{}'.format(tg)):
+    #    print p
 
     #chassis1 = root[0]
     #chassis2 = root[0][0]
