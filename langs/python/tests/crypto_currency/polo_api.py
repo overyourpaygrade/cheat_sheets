c9b2bc629f906f4c05552b7d73e75a3daaff65dc
slasheks <xhrios@gmail.com>
2016-03-18 21:36:05 -0400

    csv dump option

--- langs/python/tests/crypto_currency/polo_api.py
+++ langs/python/tests/crypto_currency/polo_api.py
@@ -37,6 +37,9 @@ def main():
         'XXBTZUSD.d',
         ]
 
+    if not args.mode:
+        parser.parser_help()
+
     # Ugly logging block
     global logger
     if args.log:
@@ -116,6 +119,25 @@ def q_asset_pairs():
 
     pprint(pairs)
 
+def excel_dump(all_pairs,coinname,currency,btc_price,val_in_usd,last_price):
+
+
+    basevol = all_pairs[coinname]['baseVolume']
+    high24 = all_pairs[coinname]['high24hr']
+    highbid = all_pairs[coinname]['highestBid']
+    low24 = all_pairs[coinname]['low24hr']
+    prcntchg = all_pairs[coinname]['percentChange']
+    quotevol = all_pairs[coinname]['quoteVolume']
+
+    with open('analysis.csv','ab+') as fh_out:
+
+        list_all = [coinname,basevol,high24,highbid,
+                    low24,prcntchg,quotevol,currency,
+                    btc_price,val_in_usd,last_price]
+        writer = csv.writer(fh_out)
+        writer.writerow(list_all)
+
+
 def analyze():
 
     logger = logging.getLogger('__analyze__')
@@ -128,19 +150,28 @@ def analyze():
 
             asset_list = list(csv.reader(fh_csv))
 
-            print "{:10} {:13} {:12} {}".format(\
-                "Coin","Polo Price","Amount","Dollars")
+            print "{:10} {:13} {:12} {:8} {}".format(\
+                "Coin","Polo Price","Amount","Dollars", "One")
 
             for coinname,shares in asset_list:
 
                 currency = float(all_pairs['{}'.format(coinname)]['last'])
                 btc_price = int(btc_usd.split('.')[0])
                 val_in_usd = (currency * float(shares)) * btc_price
+                last_price = all_pairs['{}'.format(coinname)]['last']
 
-                print "{:10} {:13} {:12} {:04.2f}".format(\
-                    coinname,all_pairs['{}'.format(coinname)]['last'],
-                    shares, val_in_usd
+                if 'USDT_BTC' in coinname:
+                    price_of_one = (currency * 1)
+                else:
+                    price_of_one = (currency * 1) * btc_price
+
+                print "{:10} {:13} {:12} {:07.2f} {:04.3f}".format(\
+                    coinname,last_price,
+                    shares, val_in_usd,price_of_one,
                     )
+
+                excel_dump(all_pairs,coinname,currency,
+                    btc_price,val_in_usd,last_price)
 
     except KeyboardInterrupt:
         exit(1)
